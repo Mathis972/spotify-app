eed705af002b0ac12740ca783094cc9a073986f3
EyZex <eyzex@hotmail.fr>
2020-10-19 23:45:52 +0200

    commit test

--- pages/index.vue
+++ pages/index.vue
@@ -1,39 +1,21 @@
 <template>
   <section class="section">
+    <h1>TST</h1>
     <div class="columns is-mobile">
-      <card
-        title="Free"
-        icon="github"
-      >
-        Open source on <a href="https://github.com/buefy/buefy">
-          GitHub
-        </a>
+      <card title="Free" icon="github">
+        Open source on <a href="https://github.com/buefy/buefy"> GitHub </a>
       </card>
 
-      <card
-        title="Responsive"
-        icon="cellphone-link"
-      >
-        <b class="has-text-grey">
-          Every
-        </b> component is responsive
+      <card title="Responsive" icon="cellphone-link">
+        <b class="has-text-grey"> Every </b> component is responsive
       </card>
 
-      <card
-        title="Modern"
-        icon="alert-decagram"
-      >
-        Built with <a href="https://vuejs.org/">
-          Vue.js
-        </a> and <a href="http://bulma.io/">
-          Bulma
-        </a>
+      <card title="Modern" icon="alert-decagram">
+        Built with <a href="https://vuejs.org/"> Vue.js </a> and
+        <a href="http://bulma.io/"> Bulma </a>
       </card>
 
-      <card
-        title="Lightweight"
-        icon="arrange-bring-to-front"
-      >
+      <card title="Lightweight" icon="arrange-bring-to-front">
         No other internal dependency
       </card>
     </div>
@@ -41,13 +23,13 @@
 </template>
 
 <script>
-import Card from '~/components/Card'
+import Card from "~/components/Card";
 
 export default {
-  name: 'HomePage',
+  name: "HomePage",
 
   components: {
-    Card
-  }
-}
+    Card,
+  },
+};
 </script>
