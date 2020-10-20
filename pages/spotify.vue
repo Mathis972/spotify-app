f72597e4efde927ee0a4c06c19df4ef5bf5e91be
EyZex <eyzex@hotmail.fr>
2020-10-20 18:24:54 +0200

    fix url2

--- pages/spotify.vue
+++ pages/spotify.vue
@@ -12,7 +12,7 @@ export default {
   methods: {
     async connect () {
 
-      await this.$axios.$get(`https://accounts.spotify.com/authorize?client_id=${process.env.spotifyId}&redirect_uri=https:%2F%2Fnuxt-spotify-stats.herokuapp.com%2Fspotify&scope=user-read-private%20user-read-email&response_type=token&state=123`, { headers: { 'Access-Control-Allow-Origin': '*' } })
+      await this.$axios.$get(`https://accounts.spotify.com/authorize?client_id=${process.env.spotifyId}&redirect_uri=https://nuxt-spotify-stats.herokuapp.com/spotify&scope=user-read-private%20user-read-email&response_type=token&state=123`, { headers: { 'Access-Control-Allow-Origin': '*' } })
     }
   }
 }
